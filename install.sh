c727e45f2d672dd1aad905a886439aae2ebb0368
sukipop <slsean@pm.me>
2024-05-04 22:27:37 -0600

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-mkdir -p "$PREFIX/share/termux-styles"
+# Function to print error message
+error() {
+    local errmsg="${1:-Unknown error}"
+    echo -e "\033[1;31mError\033[0m: ${errmsg}\n" 1>&2
+}
+
+create_share() {
+    local share_dir="$PREFIX/share/termux-styles"
+    mkdir -p "$share_dir"
+    rm -rf "$share_dir"/*
+    cp share/* "$share_dir"
+}
+
+# Ensure 'PREFIX' is defined
+if [[ -z "$PREFIX" ]]; then
+    error "Unset variable: PREFIX"
+    exit 1
+fi
+
+echo "Installing termux-textgen..."
+create_share || error
